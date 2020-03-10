af7028685f2244d4d950ad508f8e2ad629a0e202
LEANDRO ROCHA DE BRITO <00973166282@aluno.ifro.edu.br>
2020-03-09 21:36:34 -0400

    divisões

---
+++ Leandro200309.R
@@ -0,0 +1,27 @@
+#aula do dia 09/03/2020
+#Aluno: Leandro
+{
+  #divisao
+  a=15
+  b=5
+  x=a/b
+  cat("Resultado divisão:", x)
+}
+
+{
+  #divisão interira
+  a=155
+  b=5
+  x=a%/%b
+  cat("Resultado interiro:", x)
+}
+
+{
+  #
+  a=15
+  b=5
+  x=a%%b
+  cat("Resultado resto:", x)
+}
+
+
