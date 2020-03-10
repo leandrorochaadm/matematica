80543830c7ab36f31505f9c2c265eaf4661bd72a
LEANDRO ROCHA DE BRITO <00973166282@aluno.ifro.edu.br>
2020-03-09 21:55:12 -0400

    divisão real, euclidiana, resto numa unica operação

--- Leandro200309.R
+++ Leandro200309.R
@@ -5,15 +5,15 @@
   a=15
   b=5
   x=a/b
-  cat("Resultado divisão:", x)
+  cat("Resultado divisão real:", x)
 }
 
 {
-  #divisão interira
-  a=155
-  b=5
+  #exercicio 02 - divisão euclidiana
+  a=20
+  b=7
   x=a%/%b
-  cat("Resultado interiro:", x)
+  cat("Resultado divisão euclidiana é:", x)
 }
 
 {
@@ -21,7 +21,18 @@
   a=15
   b=5
   x=a%%b
-  cat("Resultado resto:", x)
+  cat("Resto da divisão é:", x)
 }
 
-
+# Implemente seu código para que ele apresente, a divisão real, euclidiana e o resto numa unica operação
+{
+  a=10
+  b=3
+  x=a/b #divisão real
+  y=a%/%b #divisão euclidiana
+  z=a%%b #resto
+  cat("RESULTADOS", "\n",
+      " Divisão real: ", x, "\n", 
+      " Divisão euclidiana: ", y, "\n", 
+      " Resto: ", z)
+}
